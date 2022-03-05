f4a33fd2b5e9e266952be9bf4aa5350695469ddb
User <masta54@mail.ru>
2022-03-05 16:36:15 +0700

    commit

---
+++ src/test/java/GitTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+public class GitTest {
+
+    @Test
+    void gitCommit() {
+        System.out.println("git commit one");
+    }
+    @Test
+    void gitCommitTwo() {
+        System.out.println("git commit two");
+    }
+    @Test
+    void gitCommitTree() {
+        System.out.println("git commit tree");
+    }
+
+}
